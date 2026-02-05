36b987d79162da038c3c5e4a0c37bb4032fa3845
Khayem Ben Ghorbel <khayembg07@gmail.com>
2026-02-06 00:25:27 +0100

    removed tests from main and added them in perspective class

--- src/Player.java
+++ src/Player.java
@@ -119,4 +119,28 @@ public class Player {
     public int hashCode() {
         return (name == null) ? 0 : name.toLowerCase().hashCode();
     }
+
+    /**
+     * Runs basic tests for World 1.
+     */
+    public void test(){
+        System.out.println("player count : "+Player.getTotalPlayers());
+        Player bob = new Player("Bob");
+        Player alice = new Player("Alice");
+        System.out.println(bob);
+        System.out.println(alice);
+        System.out.println(alice.equals("Alice")); // should be false
+        System.out.println(alice.equals(bob));     // should be false
+        Player bob2 = new Player("BOB");
+        System.out.println(bob.equals(bob2));      // should be true
+        System.out.println(bob == bob2);           // should be false
+        Player bobRef = bob;
+        System.out.println(bob == bobRef);         // should be true
+        Player p4 = new Player();
+        Player p5 = new Player();
+        System.out.println(p4);
+        System.out.println(p5);
+        System.out.println("player count : "+Player.getTotalPlayers());
+        bobRef=null;
+    }
 }
