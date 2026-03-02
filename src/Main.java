31a731eb8f36d3d797c5d44ea0d9e85339219ec7
Khayem Ben Ghorbel <khayembg07@gmail.com>
2026-03-02 15:49:47 +0100

    world 2 updates + fixes

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,9 @@ class Main{
      * main of the projects
      */
     public static void main(String[] args) {
-        //System.out.println("â¬›â¬œâ—¼\uFE0Fâ¹\uFE0FâŽ\uD83D\uDFE6\uD83D\uDFEA\uD83D\uDFEB\uD83D\uDFE9\uD83D\uDFE5");
         //TestWorld1.main();
         //TestWorld2.main();
-        Level level1 = new Level("level1.txt");
+        Level level1 = new Level("C:\\Users\\khaye\\OneDrive\\Documents\\GL1\\java\\app\\src\\level1.txt");
         level1.printGrid();
         Scanner input = new Scanner(System.in);
         while (true) {
